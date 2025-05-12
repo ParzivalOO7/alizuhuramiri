146b0cda580e1c12958b4bf14080fe334a51c1e5
Ali Zuhur Amiri <aliamiri808@yahoo.com>
2025-05-12 11:55:06 +0200

    Remove node_modules

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/alizuhuramiri/",
+  base: "/alizuhuramiri-1/",
   plugins: [react()],
 });
